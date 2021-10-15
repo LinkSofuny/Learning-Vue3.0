0aabff7e6532b11f93f1d98b649ab3b89c3cfa0c
陈渝东 <chenyud@shanhs.com>
2021-10-15 13:48:55 +0800

    feat: Vue3.0-Demo

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import DemoComponentsVue from './components/DemoComponents.vue';
+import { ref } from 'vue'
+
+const demoTitle = ref('测试')
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <input type="text" v-model='demoTitle'>
+  <DemoComponentsVue :title='demoTitle' />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DemoComponents.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script lang='ts'>
+import { ref, reactive } from "vue"
+export default {
+    props: {
+        title: String
+    },
+    setup(props: any, context: any) {
+        // console.log('props', props.title)
+
+        // Attribute (非响应式对象，等同于 $attrs)
+        console.log('Attribute', context.attrs)
+
+        // 插槽 (非响应式对象，等同于 $slots)
+        console.log('插槽', context.slots)
+
+        // 触发事件 (方法，等同于 $emit)
+        console.log('触发事件', context.emit)
+
+        // 暴露公共 property (函数)
+        console.log('暴露公共', context.expose)
+    }
+}
+</script>
+
+<style>
+
+</style>
