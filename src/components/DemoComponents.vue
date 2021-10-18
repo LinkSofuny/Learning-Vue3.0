e496660d51b7c85e3d72477c34c896a372f81507
陈渝东 <chenyud@shanhs.com>
2021-10-18 13:58:52 +0800

    feat: ts learning + template ref

--- src/components/DemoComponents.vue
+++ src/components/DemoComponents.vue
@@ -1,27 +1,38 @@
 <template>
-  <h1>{{ title }}</h1>
+  <div v-for="(item, index) in data.list" :ref=" (el: any) => { if (el) data.saveElement(el) }">this is a div element: {{ item }}</div>
 </template>
 
 <script lang='ts'>
-import { ref, reactive } from "vue"
+import { 
+    ref, 
+    watchEffect, 
+    reactive, 
+    onBeforeUpdate } from "vue"
+
 export default {
     props: {
         title: String
     },
-    setup(props: any, context: any) {
-        // console.log('props', props.title)
-
-        // Attribute (非响应式对象，等同于 $attrs)
-        console.log('Attribute', context.attrs)
-
-        // 插槽 (非响应式对象，等同于 $slots)
-        console.log('插槽', context.slots)
-
-        // 触发事件 (方法，等同于 $emit)
-        console.log('触发事件', context.emit)
-
-        // 暴露公共 property (函数)
-        console.log('暴露公共', context.expose)
+    setup() {
+        const data = reactive({
+            list: [1, 2, 3],
+            divs: [],
+            saveElement(el: HTMLDivElement): void {
+                divs[divs.length] = el
+            },
+        })
+        onBeforeUpdate (() => {
+            divs.value = []
+        }) 
+        watchEffect(() => {
+            console.log(root.value)
+        })
+        return {
+            data,
+            divs,
+            
+        }
+        // return () => <div ref={ root } />
     }
 }
 </script>
